d5e99f844f8709474724d693c609ca8be4e4a2cc
Villee00 <ville.2000.vhk@gmail.com>
2021-05-22 15:27:03 +0300

    4.7

--- osa-4/blogilista/index.js
+++ osa-4/blogilista/index.js
@@ -2,7 +2,6 @@ const http = require('http')
 const app = require('./app')
 const config = require('./utils/config')
 
-
 const server = http.createServer(app)
 
 app.listen(config.PORT, () => {
---
+++ osa-4/blogilista/utils/list_helper.js
@@ -0,0 +1,49 @@
+const lodash = require('lodash')
+
+const dummy = (blogs) => 1
+
+const totalLikes = (blogs) => {
+  const total = (sum, blog) => blog.likes + sum
+
+  return blogs.reduce(total, 0)
+}
+const favoriteBlog = (blogs) => {
+  const favorite = (top, blog) => (top.likes > blog.likes ? top
+    : { title: blog.title, author: blog.author, likes: blog.likes })
+
+  return blogs.reduce(favorite, 0)
+}
+
+const mostBLogs = (blogs) => {
+  const author = (authors, blog) => {
+    if (lodash.findIndex(authors, (o) => o.author === blog.author) !== -1) {
+      const index = lodash.findIndex(authors, (o) => o.author === blog.author)
+      authors[index].blogs += 1
+    } else {
+      authors.push({ author: blog.author, blogs: 1 })
+    }
+    return authors
+  }
+
+  return lodash.maxBy(blogs.reduce(author, []), (o) => o.blogs)
+}
+const mostLikes = (blogs) => {
+  const author = (authors, blog) => {
+    if (lodash.findIndex(authors, (o) => o.author === blog.author) !== -1) {
+      const index = lodash.findIndex(authors, (o) => o.author === blog.author)
+      authors[index].likes += blog.likes
+    } else {
+      authors.push({ author: blog.author, likes: blog.likes })
+    }
+    return authors
+  }
+
+  return lodash.maxBy(blogs.reduce(author, []), (o) => o.likes)
+}
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBLogs,
+  mostLikes,
+}
