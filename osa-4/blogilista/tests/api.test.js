1717a15f08a6172d34dc48c0d243e0ccb9bed911
Villee00 <ville.2000.vhk@gmail.com>
2021-05-24 00:28:54 +0300

    4.10 fix

--- osa-4/blogilista/tests/api.test.js
+++ osa-4/blogilista/tests/api.test.js
@@ -6,18 +6,59 @@ const helper = require('./test.helper')
 const api = supertest(app)
 const Blog = require('../models/blogi')
 
+const initalBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+  },
+  {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+  },
+  {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+  },
+]
+
 beforeEach(async () => {
   await Blog.deleteMany()
-  helper.initalBlogs.forEach(async (blog) => {
-    const blogObj = new Blog(blog)
-    await blogObj.save()
-  })
+  await Blog.insertMany(initalBlogs)
 })
 
-test('blogs are returned as json', async () => {
+test('blogs are retuned as json', async () => {
+  const response = await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+test('blogs return correct amount', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(helper.initalBlogs.length)
+  expect(response.body).toHaveLength(initalBlogs.length)
 })
 
 test('check blogs for id', async () => {
@@ -32,11 +73,9 @@ test('check blogs for id', async () => {
 test('Add blog', async () => {
   await api.post('/api/blogs')
     .send(helper.newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
 
   const response = await helper.blogsInDB()
-  expect(response).toHaveLength(helper.initalBlogs.length + 1)
+  expect(response).toHaveLength(initalBlogs.length + 1)
 })
 
 afterAll(() => {
