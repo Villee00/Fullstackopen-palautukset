340188413fbbc86834daee22331927cc86d26b9c
Villee00 <ville.2000.vhk@gmail.com>
2021-07-06 21:40:28 +0300

    9.9

---
+++ osa-9/patientor-be/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from 'cors';
+const PORT = 3001;
+const app = express();
+
+app.use(cors());
+app.get('/api/ping', (_req,res)=>{
+  res.send('pong');
+});
+
+app.listen(PORT, ()=>{
+  console.log(`app started at ${PORT}` );
+});
