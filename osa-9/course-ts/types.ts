8e891ccc922adad0f3f7e0f512004d9223596648
Villee00 <ville.2000.vhk@gmail.com>
2021-07-11 23:52:18 +0300

    -

--- osa-9/course-ts/types.ts
+++ osa-9/course-ts/types.ts
@@ -6,3 +6,32 @@ export interface courseType{
 export interface TotalProps{
   courseParts: Array<courseType>
 }
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+  
+}
+
+interface CourseNormalPart extends CoursePartBase {
+  type: "normal";
+  description: string;
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: "submission";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+interface CourseNewPart extends CoursePartBase {
+  description: string;
+}
+
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
